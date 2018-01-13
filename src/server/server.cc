6fcb4b2e7658ea964630f7fcef982ca27ccd312f
Vincent Zhao <gyroknight@users.noreply.github.com>
2018-01-13 03:33:02 -0500

    Most agent info fields added to protobuf, preliminary agent info storing function templates

--- src/server/server.cc
+++ src/server/server.cc
@@ -1,8 +1,24 @@
 #include <iostream>
 #include "src/request/request.h"
 #include "src/conn/listener.h"
+#include "src/agent"
+#include "hsa/hsa.h"
+#include "hsa/hsa_ext_finalize.h"
 
 namespace rhsa {
+
+class Agent {
+	
+	hsa_status_t storeAgent(hsa_agent_t agent, void *data) {
+		// TODO: Code to store agent info to Agent vector
+
+		return HSA_STATUS_SUCCESS;	
+	}	
+
+	void init_agents() {
+		hsa_iterate_agents(*storeAgent, NULL);
+	}
+}
 
 class Server : public ConnectionHandler {
   virtual void Handle(std::unique_ptr<Connection> conn) {
