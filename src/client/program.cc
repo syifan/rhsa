1015f8b5f92b9ec24c52c3ed347b438e7a4fa83f
Vincent Zhao <gyroknight@users.noreply.github.com>
2018-07-23 00:12:59 -0400

    Added clang-ocl, started working on GPU binary creation

---
+++ src/client/program.cc
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <cstdio>
+#include "include/hsa.h"
+#include "include/hsa_ext_finalize.h"
+
+extern "C" {
+
+hsa_status_t hsa_ext_program_create(
+    hsa_machine_model_t machine_model,
+    hsa_profile_t profile,
+    hsa_default_float_rounding_mode_t default_float_rounding_mode,
+    const char *options,
+    hsa_ext_program_t *program) {
+    return HSA_STATUS_SUCCESS;
+}
+
+hsa_status_t hsa_ext_program_add_module(
+    hsa_ext_program_t program,
+    hsa_ext_module_t module) {
+    return HSA_STATUS_SUCCESS;
+}
+
+hsa_status_t hsa_ext_program_finalize(
+    hsa_ext_program_t program,
+    hsa_isa_t isa,
+    int32_t call_convention,
+    hsa_ext_control_directives_t control_directives,
+    const char *options,
+    hsa_code_object_type_t code_object_type,
+    hsa_code_object_t *code_object) {
+    printf("%i \n", (int) program.handle);
+    printf("%i \n", (int) isa.handle);
+    printf("%i \n", call_convention);
+    printf("%s \n", options);
+    return HSA_STATUS_SUCCESS;
+}
+
+hsa_status_t hsa_ext_program_destroy(hsa_ext_program_t program) {
+    return HSA_STATUS_SUCCESS;
+}
+}
